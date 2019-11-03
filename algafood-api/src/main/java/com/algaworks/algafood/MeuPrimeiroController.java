732f0495f2255f66a6f38bca0371ba86ea99598b
Francisco <francisco.anpereira@gmail.com>
2019-11-03 17:24:37 -0300

    update project 2.14

---
+++ algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
+@RestController
+public class MeuPrimeiroController {
+
+	private AtivacaoClienteService service;
+	
+	public MeuPrimeiroController(AtivacaoClienteService service) {
+		this.service = service;
+		
+		System.out.println("MeuPrimeiroController: " + service);
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		Cliente joao = new Cliente("joao", "joao@gmail.com", "99999990");
+		service.ativar(joao);
+		return "asdasdasd!";
+	}
+}
